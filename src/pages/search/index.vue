8b931e476bb6989f62f1bc601b9cb3d2350da512
peasZY <781334498@qq.com>
2019-04-16 20:57:00 +0800

    完成搜索页面

--- src/pages/search/index.vue
+++ src/pages/search/index.vue
@@ -1,17 +1,50 @@
 <template>
   <div>
-    me
+    <!-- 搜索 -->
+    <div class="topSearch">
+      <div class="search_input">
+        <input type="text" placeholder="请输入要搜索的商品" v-model="query">
+      <icon type="search" size="16"></icon>
+      </div>
+      <div @click="ccancel" class="btncancel" v-show="query.length == 0? false : true" >取消</div>
+    </div>
+    <!-- 历史记录 -->
+    <div class="history">
+      <div class="title">
+        <p>历史搜索</p>
+        <icon type="clear" size="16"></icon>
+      </div>
+      <div class="content">
+        <div class="item">小米</div>
+        <div class="item">小米电视</div>
+        <div class="item">华为手机</div>
+        <div class="item">iphone</div>
+        <div class="item">小米音响</div>
+        <div class="item">小米扫地机器人</div>
+        <div class="item">口红</div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      query:''
+    }
+  },
+  methods: {
+    ccancel(){
+      this.query = '';
+    }
+  },
  created() {
     console.log('我的页面');
   },
 }
 </script>
 
-<style>
-
+<style lang="less">
+@import './index.less';
 </style>
